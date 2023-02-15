fa5a31dc1e0e440f11302f2c1058e7be5e7ff662
rndso15 <rndso15@naver.com>
2023-02-15 20:17:32 +0900

    feat: arc mutex map

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ extern crate tokio_tungstenite;
 
 use log::info;
 use std::{env};
+use std::collections::HashMap;
 use std::fmt::Error;
 use std::net::{IpAddr, SocketAddr};
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 use futures_util::{future, StreamExt, TryStreamExt};
-use tokio_tungstenite::tungstenite::WebSocket;
+use futures_util::stream::{SplitSink, SplitStream};
+use tokio_tungstenite::tungstenite::{Message, WebSocket};
+use tokio_tungstenite::WebSocketStream;
 
 static DEFAULT_ADDR: &str = "127.0.0.1";
 static DEFAULT_PORT: u16 = 9999;
+type ClientMap = Arc<Mutex<HashMap<i32, SplitSink<WebSocketStream<TcpStream>, Message>>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error>{
@@ -21,11 +26,15 @@ async fn main() -> Result<(), Error>{
 
     let socket_addr = SocketAddr::new(ip, DEFAULT_PORT);
 
+    let mut cnt = 0;
+    let mut clientMap: ClientMap = Arc::new(Mutex::new(HashMap::new()));
+
     let listener = TcpListener::bind(&socket_addr).await.expect("Not Bind to addr");
     while let res = listener.accept().await {
         match res {
             Ok((stream, _)) => {
-                tokio::spawn(handle_connection(stream));
+                let title = cnt.clone() + 1;
+                tokio::spawn(handle_connection(stream, title, clientMap.clone()));
             },
             Err(_) => { panic!("stream match error"); }
         }
@@ -34,7 +43,7 @@ async fn main() -> Result<(), Error>{
     Ok(())
 }
 
-async fn handle_connection(stream: TcpStream) {
+async fn handle_connection(stream: TcpStream, title: i32, mut arc: ClientMap) {
     let addr = stream.peer_addr().expect("connect failed");
     let websocket = tokio_tungstenite::accept_async(stream)
         .await
@@ -44,6 +53,12 @@ async fn handle_connection(stream: TcpStream) {
     info!("New Client In : {}", addr);
 
     let (writer, reader) = websocket.split();
+
+    // Insert Writer
+    let mut guard = arc.lock().unwrap();
+    guard.insert(title, writer);
+    println!("guard Test");
+
     reader.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
         .forward(writer)
         .await
